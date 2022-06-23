427c101ba3c5772660f0370c95a4d65aa3e37e49
Antonio Dias <antonio.dias@bit.com.br>
2022-06-23 13:36:18 -0300

    first commit, learning Design Pattern

---
+++ CursoDesignPatterns/CalculadorDeImpostos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDesignPatterns
+{
+    public class CalculadorDeImpostos
+    {
+        public void RealizaCalculo(Orcamento orcamento, string imposto)
+        {
+            if ("ICMS".Equals(imposto))
+            {
+                double icms = new Icms().CalculaIcms(orcamento);
+                Console.WriteLine(icms);
+            }
+
+            else if ("ISS".Equals(imposto))
+            {
+                double iss = new Iss().CalculaIss(orcamento);
+                Console.WriteLine(iss);
+            }
+        }
+    }
+}
---
+++ CursoDesignPatterns/Iss.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDesignPatterns
+{
+    public class Iss
+    {
+        public double CalculaIss(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.06;
+        }
+    }
+}
